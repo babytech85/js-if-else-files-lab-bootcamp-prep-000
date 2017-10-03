588a22ab110baab861d088c6df163656f2c100f9
Keyon Harris <kxh815@gmail.com>
2017-10-03 09:49:38 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -4,8 +4,9 @@ if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
+} else if (index.height == "74") 
+  console.log("Height is correct")
 }
-
 
 
 if (index.height === 70) {
